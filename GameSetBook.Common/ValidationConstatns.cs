02b1faee0e9e8e82843f974e17a3d930f96518d0
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2024-02-28 13:44:54 +0200

    ValidationConstant static Class added to the common project for Club requirements.

---
+++ GameSetBook.Common/ValidationConstatns.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSetBook.Common
+{
+    public static class ValidationConstatns
+    {
+        public static class ClubConstants
+        {
+            public const int NameMinLength = 2;
+            public const int NameMaxLength = 100;
+
+            public const int DescriptionMinLength = 10;
+            public const int DescriptionMaxLength = 1000;
+
+            public const int AddressMinLength = 5;
+            public const int AddressMaxLength = 300;
+
+            public const int EmailMinLength = 6;
+            public const int EmailMaxLength = 64;
+
+            public const int PhoneMinLength = 6;
+            public const int PhoneMaxLength = 15;
+        }
+    }
+}
