32039fed64b7ee68553503656629a7082450e114
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2024-02-28 10:12:15 +0200

    Add project files.

---
+++ GameSetBook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GameSetBook.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
