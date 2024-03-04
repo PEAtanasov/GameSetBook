0545413ea1d910ada955d878451653cff55248d4
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2024-03-04 16:53:35 +0200

    EnumExtensionAdded GetDisplayName() to display custom name given in the atribute

---
+++ GameSetBook.Infrastructure/Models/Enums/EnumExtensions/EnumExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace GameSetBook.Infrastructure.Models.Enums.EnumExtensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            var attribute = field?.GetCustomAttribute<DisplayAttribute>();
+            return attribute == null ? value.ToString() : attribute.Name?? null!;
+        }
+    }
+}
--- GameSetBook.Infrastructure/Models/Enums/Surface.cs
+++ GameSetBook.Infrastructure/Models/Enums/Surface.cs
@@ -1,11 +1,22 @@
-﻿namespace GameSetBook.Infrastructure.Models.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameSetBook.Infrastructure.Models.Enums
 {
     public enum Surface
     {
+        [Display(Name = "Grass")]
         Grass = 0,
+
+        [Display(Name = "Artificial Grass")]
         ArtificialGrass = 1,
+
+        [Display(Name = "Hard")]
         Hard = 2,
+
+        [Display(Name = "Clay")]
         Clay = 3,
+
+        [Display(Name = "Carpet")]
         Carpet = 4
     }
 }
