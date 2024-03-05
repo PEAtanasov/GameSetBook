4f387f0865f1df193399ff71a231a42f3754d11a
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2024-03-05 22:07:35 +0200

    Configuring added in the onModelCreating method in the ApplicationDbConntext. Must move them to Configuration folder later.

--- GameSetBook.Infrastructure/Data/ApplicationDbContext.cs
+++ GameSetBook.Infrastructure/Data/ApplicationDbContext.cs
@@ -15,11 +15,15 @@ namespace GameSetBook.Infrastructure.Data
         }
 
         public DbSet<Booking> Bookings { get; set; } = null!;
-        public DbSet<Club> Clubs { get; set; } = null!;
-        public DbSet<Court> Courts { get; set; } = null!;
-        public DbSet<GameSetMatchUpPlayerProfile> GameSetMatchUpProfiles { get; set; } = null!;
         public DbSet<City> Cities { get; set; } = null!;
+        public DbSet<Club> Clubs { get; set; } = null!;
+        public DbSet<ClubReview> ClubReviews { get; set; } = null!;
         public DbSet<Country> Countries { get; set; } = null!;
+        public DbSet<Court> Courts { get; set; } = null!;
+        public DbSet<GameSetMatchUpPlayerProfile> GameSetMatchUpPlayerProfiles { get; set; } = null!;
+        public DbSet<Message> Messages { get; set; } = null!;
+        public DbSet<Tournament>  Tournaments { get; set; } = null!;
+        public DbSet<TournamentGSMUPlayerProfile> TournamentsGSMUPlayerProfiles { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -29,12 +33,24 @@ namespace GameSetBook.Infrastructure.Data
             builder.Entity<TournamentGSMUPlayerProfile>()
                 .HasOne(tp => tp.PlayerProile)
                 .WithMany(pp => pp.TournamentsGSMUPlayerProfile)
-                .OnDelete(DeleteBehavior.NoAction); // Set OnDelete behavior to Cascade
+                .OnDelete(DeleteBehavior.Restrict); // Set OnDelete behavior to Cascade, or noaction
 
             builder.Entity<TournamentGSMUPlayerProfile>()
                 .HasOne(tp => tp.Tournament)
                 .WithMany(t => t.TournamentGSMUPlayerProfiles)
                 .OnDelete(DeleteBehavior.NoAction);
+
+            builder.Entity<GameSetMatchUpPlayerProfile>()
+                .HasMany(p => p.SentMessages)
+                .WithOne(m => m.SenderProfile)
+                .HasForeignKey(m => m.SenderProfileId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<GameSetMatchUpPlayerProfile>()
+                .HasMany(p => p.ReceivedMessages)
+                .WithOne(m => m.ReceiverProfile)
+                .HasForeignKey(m => m.ReceiverProfileId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             base.OnModelCreating(builder);
         }
