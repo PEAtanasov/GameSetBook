d08c0e19aa3d91a73fb6dc8967786a7ee53894c3
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2024-03-05 13:45:55 +0200

    PlayStyle enums added to describe player's style of play. PlayerLevel enum added to describe players level of playing tennis.

---
+++ GameSetBook.Infrastructure/Models/Enums/PlayStyle.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameSetBook.Infrastructure.Models.Enums
+{
+    public enum PlayStyle
+    {
+        [Display(Name = "No style")]
+        None = 0,
+
+        [Display(Name = "Baseline Player")]
+        Baseliner = 1,
+
+        [Display(Name = "All-Court Player")]
+        AllCourtPlayer = 2,
+
+        [Display(Name = "Serve-and-Volley Player")]
+        ServeAndVolleyPlayer = 3,
+
+        [Display(Name = "Counterpuncher")]
+        Counterpuncher = 4,
+
+        [Display(Name = "Power Player")]
+        PowerPlayer = 5,
+
+        [Display(Name = "Tactical Player")]
+        TacticalPlayer = 6
+    }
+}
+
