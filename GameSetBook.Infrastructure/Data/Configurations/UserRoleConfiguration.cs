7498811395e420b371f84975613db80782a4221e
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2024-03-09 18:15:28 +0200

    .

--- GameSetBook.Infrastructure/Data/Configurations/UserRoleConfiguration.cs
+++ GameSetBook.Infrastructure/Data/Configurations/UserRoleConfiguration.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GameSetBook.Infrastructure.Data.Configurations
 {
