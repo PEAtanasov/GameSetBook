fae206f4e75c33c1e78942d2a6343a7f88d3bced
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2024-04-04 16:47:53 +0300

    JS script added to submit form with Id-filterForm when any date is selected from  input type="date"

--- GameSetBook/wwwroot/js/site.js
+++ GameSetBook/wwwroot/js/site.js
@@ -12,3 +12,13 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var datepickerElements = document.querySelectorAll('#filterForm input[type="date"]');
+
+    datepickerElements.forEach(function (input) {
+        input.addEventListener('change', function () {
+            document.getElementById('filterForm').submit();
+        });
+    });
+});
