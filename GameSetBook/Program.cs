93f2f6153a0d5c5d54fe5077b840a4c7bf107293
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2024-04-15 22:05:59 +0300

    Files in View folder cleared from commented code

--- GameSetBook/Program.cs
+++ GameSetBook/Program.cs
@@ -17,18 +17,16 @@ builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    //app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
+    
     app.UseMigrationsEndPoint();
 }
 else
 {
     app.UseExceptionHandler("/Home/Error/500");
     app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
